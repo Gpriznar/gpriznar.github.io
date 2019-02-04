1b9154b4a24381860bc0480d849afceb1e752975
Gpriznar <44954435+Gpriznar@users.noreply.github.com>
2019-02-04 16:13:21 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+//Image switch 
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/GP.jpeg') {
+      myImage.setAttribute ('src','images/Sunglasses.jpeg');
+    } else {
+      myImage.setAttribute ('src','images/GP.jpeg');
+    }
+}
+
+// Welcome Message
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Welcome ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
